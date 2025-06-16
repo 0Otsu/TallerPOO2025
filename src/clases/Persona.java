f3257c52b2cc651e2f6a72f13017f70263197e66
augusto <augustovlean@gmail.com>
2025-06-16 15:22:02 -0300

    Modifico Persona.java: agrego atributos y sus métodos

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -1,3 +1,26 @@
-public class Persona {
+public abstract class Persona {
+    private long dni;
     private String nombre;
+    private String apellido;
+
+    public Persona(long dni, String nombre, String apellido) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public long getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public abstract String toString(); // Forzamos a las subclases a definir cómo se muestran
+
 }
