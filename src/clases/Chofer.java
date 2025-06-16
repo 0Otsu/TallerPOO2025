d6d6a4a26c023033b082436d30206f007b33e71f
augusto <augustovlean@gmail.com>
2025-06-16 15:31:26 -0300

    Agrego clases Persona, Chofer y la enumeración Categoria

--- src/clases/Chofer.java
+++ src/clases/Chofer.java
@@ -1,3 +1,16 @@
-public class Chofer {
-    
+public class Chofer extends Persona {
+    private String nroLicencia;
+    private Categoria categoria;
+
+    public Chofer(long dni, String nombre, String apellido, String nroLicencia, Categoria categoria) {
+        super(dni, nombre, apellido);
+        this.nroLicencia = nroLicencia;
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Chofer: " + getNombre() + " " + getApellido() + ", DNI: " + getDni() +
+                ", Licencia: " + nroLicencia + ", Categoría: " + categoria;
+    }
 }
